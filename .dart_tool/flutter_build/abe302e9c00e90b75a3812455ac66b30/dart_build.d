d44620b74713b49f96ef24272f282595b1a3dd83
Apih SkiBiDi <hafizmustaqim00@gmail.com>
2025-02-19 10:12:41 +0800

    Initial commit

---
+++ .dart_tool/flutter_build/abe302e9c00e90b75a3812455ac66b30/dart_build.d
@@ -0,0 +1,1 @@
+ C:\\Users\\hafiz\\OneDrive\\Desktop\\carea_app\\.dart_tool\\flutter_build\\abe302e9c00e90b75a3812455ac66b30\\dart_build_result.json: 
---
+++ .dart_tool/flutter_build/abe302e9c00e90b75a3812455ac66b30/install_code_assets.d
@@ -0,0 +1,1 @@
+ C:\\Users\\hafiz\\OneDrive\\Desktop\\carea_app\\.dart_tool\\flutter_build\\abe302e9c00e90b75a3812455ac66b30\\native_assets.json: 
